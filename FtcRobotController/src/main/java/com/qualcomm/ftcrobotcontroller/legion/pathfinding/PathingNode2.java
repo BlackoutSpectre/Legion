6eb8cc899b889ea7525f0f421ff1c753c1628597
BlackoutSpectre <Blackout.Spectre.10906@gmail.com>
2016-01-02 17:39:54 -0600

    updated pathfinding nodes to be compatible to the configuration.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/pathfinding/PathingNode2.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/pathfinding/PathingNode2.java
@@ -1,26 +1,21 @@
 package com.qualcomm.ftcrobotcontroller.legion.pathfinding;
 
 import com.qualcomm.ftcrobotcontroller.legion.Navigation;
-import com.qualcomm.ftcrobotcontroller.legion.Wall;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 /**
  * Created by Michael Chick on 7/15/2015.
- *
- * Note to programmer: when seeing a new obstruction, check to see if it's static first and if it's
- * a ramp, if not, call the newTime() method and then update the path. The pathfinding algorithms will
- * automatically check to see if the obstruction is still present.
+ * @deprecated
  */
-public class PathingNode extends AbstractNode {
+public class PathingNode2 extends AbstractNode {
 
     private int xPos;
     private int yPos;
     public Navigation nav;
-    long timeSeen = 0;
 
-    public PathingNode(int xPosition, int yPosition) {
+    public PathingNode2(int xPosition, int yPosition) {
         super(xPosition, yPosition);
         // do other init stuff
 
@@ -86,17 +81,13 @@ public class PathingNode extends AbstractNode {
     {
         return obstructionPresent();
     }
-    /**returns if there is a "ramp" here or not. Used if you don't want the robot to get confused
+    /*returns if there is a "ramp" here or not. Used if you don't want the robot to get confused
       due to an incline in sight.
       Postcondition: returns true if a ramp was present.
      */
     public boolean isRamp()
     {
         return ramp;
-    }
-    public void setRamp(boolean isRamp)
-    {
-        ramp=isRamp;
     }
 
     /*Precondition: if an obstruction was detected.
@@ -114,16 +105,13 @@ public class PathingNode extends AbstractNode {
 
     /**
      * Sets up a new obstruction and set the map as unwalkable, for a duration of EXPIRATION milliseconds.
-     * @deprecated
+     *
      */
     private void seenNewObstruction()
     {
         {
-            if (isStatic())
-                return;
-
             setWall(true);
-
+            timer.schedule(countdown, EXPIRATION);
            //update pathing
         }
     }
@@ -134,9 +122,9 @@ public class PathingNode extends AbstractNode {
      * detected obstructions will be removed after this many milliseconds.
      */
     static final long EXPIRATION = 5000; //detected obstructions will be removed after this many milliseconds.
-    //private Timer timer = new Timer();
-    //private TimerTask countdown = new TempWall();
-    /*class TempWall extends TimerTask
+    private Timer timer = new Timer();
+    private TimerTask countdown = new TempWall();
+    class TempWall extends TimerTask
     {
 
         @Override
@@ -146,10 +134,10 @@ public class PathingNode extends AbstractNode {
             setWall(false);
 
         }
-    }*/
+    }
 
     private boolean isStatic;
-   /* public Wall(long timeSeen, boolean isStatic)
+    /*public Wall(long timeSeen, boolean isStatic)
     {
         if (isStatic)
             timeSeen = -1;
@@ -161,57 +149,42 @@ public class PathingNode extends AbstractNode {
     /**updates the time in which another obstruction at this point was seen
      as long it is not a static wall.
      */
-    public void newTime(long timeSeen)
+    /*public void newTime(long timeSeen)
     {
         if (!isStatic())
             this.timeSeen=timeSeen;
-    }
+    }*/
 
     public void newTime()
     {
         if (!isStatic())
-            newTime(System.currentTimeMillis());
+            seenNewObstruction();
     }
 
-    /**tells you if the wall will always be there or not (to be static or not to be static).
+    /*tells you if the wall will always be there or not (to be static or not to be static).
      */
     public boolean isStatic()
     {
         return isStatic;
     }
 
-
-    /*private boolean obstructionPresent()
+    /*returns if the is an obstruction present at this point.*/
+    private boolean obstructionPresent()
     {
         return !isWalkable();
-    }*/
-    /**returns if the is an obstruction present at this point.*/
-    public boolean obstructionPresent()
+    }
+    /*public boolean obstructionPresent()
     {
         if (isStatic())
-        {
             return true;
-        }
-
         else if (timeSeen+EXPIRATION<=System.currentTimeMillis())
             return true;
         return false;
-    }
-
-    /**
-     * instead of getting an inaccurate answer from the super class, the walkability of the timeout
-     * will be considered
-     * @return if the tile is walkable
-     */
-    @Override
-    public boolean isWalkable() {
-        return !obstructionPresent();
-
-    }
+    }*/
 
     private void setWall(boolean present)
     {
-        super.setWalkable(!present);
+        setWalkable(!present);
 
     }
 
