032e96f4f2cf3c880435a2ab3bb6b3c6d0076cb7
BlackoutSpectre <Blackout.Spectre.10906@gmail.com>
2016-01-02 15:48:55 -0600

    building functionality to MapCompiler

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/Configuration/MapCompiler.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/Configuration/MapCompiler.java
@@ -6,13 +6,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
 import com.qualcomm.ftcrobotcontroller.R;
 import com.qualcomm.ftcrobotcontroller.legion.pathfinding.Grid;
 import com.qualcomm.ftcrobotcontroller.legion.pathfinding.Map;
 import com.qualcomm.ftcrobotcontroller.legion.pathfinding.PathingNode;
 
-import com.nostra13.universalimageloader.*;
 
 import java.io.File;
 
@@ -22,12 +23,17 @@ public class MapCompiler extends Activity {
     private Map<PathingNode> actualTileGrid;
     private Map<PathingNode> scaledTileGrid;
     private File baseConfigFolder;
+    ImageLoader imageLoader;
+    ProgressBar progressBar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_compiler);
+        imageLoader = ImageLoader.getInstance();
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
 
     @Override
