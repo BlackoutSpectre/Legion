19a2cef3632730b64f65ad7ff6bef78781628ab1
BlackoutSpectre <Blackout.Spectre.10906@gmail.com>
2016-01-02 21:30:42 -0600

    build some more functionality to the compiler

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/Configuration/MapCompiler.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/Configuration/MapCompiler.java
@@ -2,13 +2,23 @@ package com.qualcomm.ftcrobotcontroller.legion.Configuration;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingProgressListener;
 import com.qualcomm.ftcrobotcontroller.R;
 import com.qualcomm.ftcrobotcontroller.legion.Helper;
 import com.qualcomm.ftcrobotcontroller.legion.pathfinding.Grid;
@@ -17,6 +27,8 @@ import com.qualcomm.ftcrobotcontroller.legion.pathfinding.PathingNode;
 
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class MapCompiler extends Activity {
 
@@ -26,6 +38,10 @@ public class MapCompiler extends Activity {
     private File baseConfigFolder;
     ImageLoader imageLoader;
     ProgressBar progressBar;
+    ImageView bitmapPreview;
+    TextView statusText;
+    BitmapFactory.Options options;
+    Bitmap bitmap = null;
 
 
     @Override
@@ -35,7 +51,16 @@ public class MapCompiler extends Activity {
         baseConfigFolder = Helper.getBaseFolder();
         imageLoader = ImageLoader.getInstance();
 
+
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        bitmapPreview = (ImageView) findViewById(R.id.imageView);
+        statusText = (TextView) findViewById(R.id.status_message_text);
+        options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
     }
 
     @Override
@@ -62,6 +87,55 @@ public class MapCompiler extends Activity {
 
     public void loadBitmap(View view)
     {
+        File bitmapFile = new File(Helper.getBaseFolder(),Helper.bitmapImageFilename);
+        try {
+            FileInputStream fileInputStream = new FileInputStream(bitmapFile);
+            bitmap = BitmapFactory.decodeStream(fileInputStream);
+            bitmapPreview.setMaxHeight(bitmap.getHeight());
+            bitmapPreview.setMaxWidth(bitmap.getWidth());
+            bitmapPreview.setImageBitmap(bitmap);
+
+
+
+        } catch (FileNotFoundException e) {
+            Toast.makeText(getApplicationContext(),"File not found: "+bitmapFile.toString(),Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+        /*DisplayImageOptions options = new DisplayImageOptions.Builder().build();
+        ImageLoaderConfiguration imageLoaderConfiguration = new ImageLoaderConfiguration.Builder(getBaseContext()).build();
+        imageLoader.init(imageLoaderConfiguration);
+        imageLoader.displayImage(baseConfigFolder.toString() + Helper.bitmapImageFilename, bitmapPreview, options,
+                new ImageLoadingListener() {
+                    @Override
+                    public void onLoadingStarted(String s, View view) {
+
+                    }
+
+                    @Override
+                    public void onLoadingFailed(String s, View view, FailReason failReason) {
+
+                    }
+
+                    @Override
+                    public void onLoadingComplete(String s, View view, Bitmap bitmap) {
+                        bitmapPreview.setMaxHeight(bitmap.getHeight());
+                        bitmapPreview.setMaxWidth(bitmap.getWidth());
+                        bitmapPreview.setImageBitmap(bitmap);
+                        setBitmap(bitmap);
+                    }
+
+                    @Override
+                    public void onLoadingCancelled(String s, View view) {
+
+                    }
+                }, new ImageLoadingProgressListener(){
+                    @Override
+                    public void onProgressUpdate(String s, View view, int i, int i1) {
+                        progressBar.setMax(i1);
+                        progressBar.setProgress(i);
+                    }
+        });*/
 
     }
 }
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/Helper.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/Helper.java
@@ -11,6 +11,7 @@ import java.io.File;
  */
 public class Helper
 {
+    public static final String bitmapImageFilename = "/map.bmp";
 
     /**
      * gets the path to the configuration folder, if the path doesn't exist, then it will generate
