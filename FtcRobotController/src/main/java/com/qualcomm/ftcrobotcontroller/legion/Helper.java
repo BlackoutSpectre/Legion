79ff1c2a8311000247321cfa2d512395b12724b0
BlackoutSpectre <Blackout.Spectre.10906@gmail.com>
2016-01-09 16:29:41 -0600

    added .nomedia method

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/Helper.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/Helper.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.os.Environment;
 import android.widget.Toast;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by Blackout Spectre on 12/18/2015.
@@ -37,6 +40,17 @@ public class Helper
         }
         else
             return null;
+    }
+    public static void makeNoMediaFile()
+    {
+        File baseFolder = getBaseFolder();
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(new File(baseFolder, "/.nomedia")));
+            writer.write("");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     public static class UI
     {
