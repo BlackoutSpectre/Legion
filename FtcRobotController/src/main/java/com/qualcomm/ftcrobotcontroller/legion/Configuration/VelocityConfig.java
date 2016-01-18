29d15135ab63e3b64f22bc404a8800f12db9b168
BlackoutSpectre <Blackout.Spectre.10906@gmail.com>
2016-01-18 13:14:21 -0600

    started building config code

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/Configuration/VelocityConfig.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/legion/Configuration/VelocityConfig.java
@@ -4,15 +4,36 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.qualcomm.ftcrobotcontroller.R;
+import com.qualcomm.ftcrobotcontroller.legion.Navigation;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class VelocityConfig extends Activity {
+    private EditText leftRadiusBox;
+    private EditText rightRadiusBox;
+    private EditText leftCircumBox;
+    private EditText rightCircumBox;
+    private File configFileDir = Navigation.WHEEL_CONFIG_FILE_DIR;
+    private double leftRadius=-1;
+    private double rightRadius=-1;
+    private double leftCircum=-1;
+    private double rightCircum=-1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_velocity_config);
+
+        if (configExists())
+        {
+            loadConfig();
+        }
     }
 
     @Override
@@ -35,5 +56,34 @@ public class VelocityConfig extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean configExists()
+    {
+        return configFileDir.exists();
+    }
+
+    private void loadConfig()
+    {
+        if (configExists())
+        {
+            try {
+                Scanner scanner = new Scanner(configFileDir);
+                leftRadius = scanner.nextDouble();
+                rightRadius = scanner.nextDouble();
+                updateFromRadius();
+            } catch (FileNotFoundException e) {
+                Toast.makeText(getApplicationContext(),"Error finding config file: "+configFileDir.getPath()
+                ,Toast.LENGTH_LONG).show();
+
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+
+    private void updateFromRadius()
+    {
+
     }
 }
